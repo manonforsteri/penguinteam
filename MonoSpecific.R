8bb05a2ee9fba03e982639cbb0f8261b0fd73a1d
timotheenivalis <timothee.bonnet@ieu.uzh.ch>
2015-03-24 13:27:45 +0100

    monospe in one file

--- MonoSpecific.R
+++ MonoSpecific.R
@@ -1,3 +1,102 @@
+#----------------------------------------------------------
+#--- modele de cormack-jolly-seber multi-site multi-espece
+#----------------------------------------------------------
+rm=list(ls())
+
+setwd('C:/Users/Manon Ghislain L/Documents/Scripts/JAGS')
+
+#---- charge les packages dont on aura besoin
+library(R2jags) # pour appeler jags depuis R
+library(FSA) # pour calculer les m-array (statistiques reduites)
+library(abind) # pour combiner des arrays (generalise r/cbind)
+
+#---- lit les donnees et les explore
+dat <- read.table('datadefinitivesTURMER.txt',header=T,stringsAsFactors=T)
+
+head(dat)
+tail(dat)
+names(dat)
+dim(dat)
+
+#dat[,"X.COV.espece"]<-NULL
+
+attach(dat)
+
+sum(S.) # nb total d individus
+levels(X.COV.site) # modalites var site
+levels(X.COV.sexe) # modalites var sexe
+levels(X.COV.espece) # modalites var espece
+nsites = length(levels(X.COV.site)) # nb sites 
+nsex =  length(levels(X.COV.sexe)) # nb sexes
+sexes<-levels(X.COV.sexe) # modalites var sexe
+
+#---- met les donnees dans un format utilisable pour l analyse bayesienne
+# pre-allocation memoire 
+lsex = vector()
+lsite = vector()
+N = NULL # liste des marray par espece x site
+
+uni<-1#unique marray index
+M = list() # pre-alloue memoire pour les marray du sex courant
+ind = 1
+for (sex in 1:length(sexes))
+{
+  focsex <- sexes[sex]# recupere nom sex courant
+  tempsex<-subset(dat,X.COV.sexe == focsex)
+  for (s in 1:nsites)# boucle sur les sites
+  { 
+    i = levels(X.COV.site)[s] # recupere nom site courant
+    temp <- subset(tempsex, X.COV.site == i) # selectionne le jeu de donnees pour site i espece k
+    
+    # les histoires sont groupees, degroupe pour revenir au format une ligne est un individu
+    temp.converted <- capHistConvert(temp,freq="S.",in.type="frequency",out.type="individual")
+    # convertit les histoires de capture en un m-array
+    m.array.temp <- capHistSum(temp.converted,cols2use=-c(26:28))$m.array
+    m.array.temp[is.na(m.array.temp)] = 0 # on remplace les NA par des 0
+    m.array.temp <- m.array.temp[,-1] # on enleve la col nb de relaches
+    #rowSums(m.array.temp) # pour info, calcule le nb de relaches par annee
+    M[[ind]]= m.array.temp # stocke le marray pour le site courant dans la liste M
+    lsex[uni]<-focsex
+    lsite[uni]<-i
+    ind <- ind+1
+    uni <- uni+1
+  }# end for (s in 1:nsites)
+  lgr <- length(M) # stocke le nb de sites*sexes de l espece courante
+  N <- c(N,M) # concatene les sites de l espece courante avec ceux des especes precedentes
+}#end for (sex in 1:length(sexes))
+#lgr[j] <- length(M) # stocke le nb de sites*sexes de l espece courante
+#N <- c(N,M) # concatene les sites de l espece courante avec ceux des especes precedentes
+
+# transforme la liste en array qui sera lu par JAGS
+Marray=abind(M, along = 3)
+dim(Marray) # 25 25  318 // 318 sites avec pour chacun un marray de dim 25x25
+
+nsp<-1
+vecsp = matrix(NA,nrow=max(lgr),ncol=1)
+somcum = cumsum(lgr)
+temp = 1:somcum[1]
+vecsp[1:length(temp),1] = temp
+
+
+#Si on suppose que dans I, m?me proportion de sexes que dans datas identifi?es
+M<-dat[which(dat[,"X.COV.sexe"]=="M"),]
+MALE<-sum(M[,"S."])
+ALL<-sum(S.)
+PROPMALES<-MALE/ALL
+
+slopeLsex<-lsex
+slopeLsex[which(slopeLsex=="I")]<-PROPMALES-0.5
+slopeLsex[which(slopeLsex=="M")]<-0.5
+slopeLsex[which(slopeLsex=="F")]<- -0.5
+
+lsite<-rep(1: nsites, nsex)
+#Si on suppose que dans I, 50% M et 50%F
+#slopeLsex<-(as.numeric(as.factor(lsex))-2)/2 # -0.5 pour femelles, 0.5 pour males, et 0 pour indetermines (suppose un melange proche de 50%)
+
+save.image(file = "TURMERarraysloaded.RData")
+# on charge le package qui permet d appeler jags depuis R
+library(R2jags)
+
 ### BUGS analysis
 
 sink("cjs-mnl.jags")
@@ -61,7 +160,7 @@ nbmarrays<-dim(Minput)[3]
 nsitesInput<-nbmarrays/2
 lsiteInput<-rep(c(1:10),2)
 
-#avec jeu de données partiel:
+#avec jeu de donn?es partiel:
 jags.data <- list(marr = Minput, n.occasions = dim(Minput[,,1])[2], 
                   rel = apply(Minput,3,rowSums),
                   nbmarrays=nbmarrays,
