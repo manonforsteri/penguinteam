5ef8d2133145bd85f2f9e9c0d5ac10e9bdb82597
timotheenivalis <timothee.bonnet@ieu.uzh.ch>
2015-07-17 17:47:03 +0200

    Small example of interaction fitting on simul

--- SimulatingData.R
+++ SimulatingData.R
@@ -1,11 +1,12 @@
 setwd(dir = "C:/Users/Thimothee Admin/Documents/GitHub/penguinteam/")
+library(R2jags)
 
 ###### Simulation model phi(sp+t) p(.) #####
-n.occasions <- 15                   # Number of capture occasions
+n.occasions <- 6                   # Number of capture occasions
 meanphi<-0#logit intercept survival (i.e 0.5 on survival scale)
 effect_Occasions<-c(0,rnorm(n = n.occasions-2,mean = 0,sd = 0.5))#time effect
 
-n.sp<-5
+n.sp<-4
 effect_Sp<-c(0,rnorm(n = n.sp-1,mean=0,sd=1))
 
 
@@ -87,7 +88,7 @@ cat("
     # Priors and constraints
     for (i in 1:nind){
       for (t in f[i]:(n.occasions-1)){
-        logit(phi[i,t]) <- mu.phi + beta.t[t] + beta.sp[sp_ind[i]]
+        logit(phi[i,t]) <- mu.phi + beta.t[t] + beta.sp[ sp_ind[i] ]
         logit(p[i,t]) <- mu.p
       } #t
     } #i
@@ -127,26 +128,98 @@ cat("
     ",fill = TRUE)
 sink()
 
+
+sink("cjs-phi(spXt)p(.).jags")
+cat("
+    model {
+    # Priors and constraints
+    for (i in 1:nind){
+    for (t in f[i]:(n.occasions-1)){
+    logit(phi[i,t]) <- mu.phi + beta.t[t] + beta.sp[ sp_ind[i] ] + beta.tXsp[ t,sp_ind[i] ]
+    logit(p[i,t]) <- mu.p
+    } #t
+    } #i
+    
+    meanphi ~ dunif(0, 1)              # Priors for group-specific survival
+    
+    mu.phi<- log(phi.mean/(1-phi.mean))
+    phi.mean ~ dunif(0,1)
+    
+    beta.t[1]<-0
+    beta.tXsp[1,1]<-0
+    for (t in 2:(n.occasions-1)){
+    beta.t[t] ~ dnorm(0,0.01)
+    beta.tXsp[t,1]<-0
+    }
+    
+    beta.sp[1]<-0
+    for (sp in 2:n.sp){
+    beta.sp[sp] ~ dnorm(0,0.01)
+    beta.tXsp[1,sp]<-0
+    for (t in 2:(n.occasions-1)){
+    beta.tXsp[t,sp] ~ dnorm(0,0.01)
+    }
+    }
+    
+    mu.p <- log(p.mean/(1-p.mean))
+    p.mean ~ dunif(0,1)
+    
+    # Likelihood 
+    for (i in 1:nind){
+    # Define latent state at first capture
+    z[i,f[i]] <- 1
+    for (t in (f[i]+1):n.occasions){
+    # State process
+    z[i,t] ~ dbern(mu1[i,t])
+    mu1[i,t] <- phi[i,t-1] * z[i,t-1]
+    # Observation process
+    y[i,t] ~ dbern(mu2[i,t])
+    mu2[i,t] <- p[i,t-1] * z[i,t]
+    } #t
+    } #i
+    }
+    ",fill = TRUE)
+sink()
+
 # Bundle data
 jags.data <- list(y = CH, f = f, nind = dim(CH)[1], n.occasions = dim(CH)[2], z = known.state.cjs(CH), sp_ind=sp_ind, n.sp=length(unique(sp_ind)) )
 
-# Initial values
+#### Initial values####
+#interaction init
+
+beta.tXsp_ini<-function()
+{beta.tXsp<-matrix(NA,nrow = n.occasions-1,ncol = n.sp)
+for (i in 2:(n.occasions-1))
+  {
+    for (j in 2:n.sp)
+      {
+        beta.tXsp[i,j]<-rnorm(n = 1,mean = 0,sd = 1)
+      }
+  }
+return(beta.tXsp)
+}
+
 inits <- function(){list(z = cjs.init.z(CH, f), phi.mean = runif(1, 0, 1), p.mean = runif(1, 0, 1),
                          beta.t=c(NA,rnorm(n = n.occasions-2,mean = 0,sd=1 ) ),
-                         beta.sp=c(NA,rnorm(n = n.sp-1,mean = 0,sd=1 ) ))}  
+                         beta.sp=c(NA,rnorm(n = n.sp-1,mean = 0,sd=1 ) ),
+                         beta.tXsp= beta.tXsp_ini() )}  
 
 # Parameters monitored
-parameters <- c("phi.mean", "p.mean","beta.t","beta.sp")
+parameters <- c("phi.mean", "p.mean","beta.t","beta.sp","beta.tXsp")
 
 # MCMC settings
-ni <- 1000
-nt <- 3
-nb <- 500
+ni <- 11000
+nt <- 10
+nb <- 1000
 nc <- 3
 
-# Call JAGS from R (BRT 2 min)
-cjs.group <- jags(jags.data, inits, parameters, "cjs-phi(sp+t)p(.).jags",
+# Call JAGS from R 
+cjs.additif <- jags(jags.data, inits, parameters, "cjs-phi(sp+t)p(.).jags",
                   n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb, working.directory = getwd())
+print(cjs.additif, digits = 3)
 
+cjs.interaction <- jags(jags.data, inits, parameters, "cjs-phi(spXt)p(.).jags",
+                        n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb, working.directory = getwd())
+print(cjs.interaction, digits = 3)
 # Summarize posteriors
-print(cjs.group, digits = 3)
+
